34b5d0044c00c174e9d72a19b6ecc1bf20802528
Richard Smith <richardsmith.digitaldwp@gmail.com>
2021-01-20 15:53:02 +0000

    Routing for eligible/not eligible v1

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,27 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/v1/eligible', (req, res, next) => {
+  const whereLived = req.session.data['where-lived'];
+  if (whereLived === 'No') {
+    res.redirect('/v1/not-eligible');
+  } else {
+    next();
+  }
+
+  const condition = req.session.data['condition'];
+  if (condition === 'No') {
+    res.redirect('/v1/not-eligible');
+  } else {
+    next();
+  }
+
+  const over16 = req.session.data['over-16'];
+  if (over16 === 'No') {
+    res.redirect('/v1/not-eligible');
+  } else {
+    next();
+  }
+});
+
  module.exports = router
