b5107f0bc02c51ef1d84fadab8038da41e517df8
Richard Smith <richardsmith.digitaldwp@gmail.com>
2021-01-25 15:46:20 +0000

    Updating new routes file v0-1

--- app/routes.js
+++ app/routes.js
@@ -41,35 +41,35 @@ router.post('/poc/contact-details', (req, res, next) => {
 
 // ROUTES V0-1 START
 
-router.post('/poc/eligible', (req, res, next) => {
+router.post('/v0-1/eligible', (req, res, next) => {
   const whereLived = req.session.data['where-lived'];
   if (whereLived === 'No') {
-    res.redirect('/poc/not-eligible');
+    res.redirect('/v0-1/not-eligible');
   } else {
     next();
   }
 
   const condition = req.session.data['condition'];
   if (condition === 'No') {
-    res.redirect('/poc/not-eligible');
+    res.redirect('/v0-1/not-eligible');
   } else {
     next();
   }
 
   const over16 = req.session.data['over-16'];
   if (over16 === 'No') {
-    res.redirect('/poc/not-eligible');
+    res.redirect('/v0-1/not-eligible');
   } else {
     next();
   }
 });
 
-router.post('/poc/contact-details', (req, res, next) => {
+router.post('/v0-1/contact-details', (req, res, next) => {
  const safeAddress = req.session.data['safe-address'];
  if (safeAddress === 'No') {
-   res.redirect('/poc/address-other');
+   res.redirect('/v0-1/address-other');
  } else {
-   res.redirect('/poc/contact-details');
+   res.redirect('/v0-1/contact-details');
  }
  });
  // ROUTES V0-1 END
