0440eb66d4de2a3901a0a0f95177b5e75b4dc995
Richard Smith <richardsmith.digitaldwp@gmail.com>
2021-01-25 11:17:24 +0000

    Creating v0-1

--- app/routes.js
+++ app/routes.js
@@ -37,6 +37,41 @@ router.post('/poc/contact-details', (req, res, next) => {
  }
  });
 
-// ROUTES poc END
+// ROUTES  END
+
+// ROUTES V0-1 START
+
+router.post('/poc/eligible', (req, res, next) => {
+  const whereLived = req.session.data['where-lived'];
+  if (whereLived === 'No') {
+    res.redirect('/poc/not-eligible');
+  } else {
+    next();
+  }
+
+  const condition = req.session.data['condition'];
+  if (condition === 'No') {
+    res.redirect('/poc/not-eligible');
+  } else {
+    next();
+  }
+
+  const over16 = req.session.data['over-16'];
+  if (over16 === 'No') {
+    res.redirect('/poc/not-eligible');
+  } else {
+    next();
+  }
+});
+
+router.post('/poc/contact-details', (req, res, next) => {
+ const safeAddress = req.session.data['safe-address'];
+ if (safeAddress === 'No') {
+   res.redirect('/poc/address-other');
+ } else {
+   res.redirect('/poc/contact-details');
+ }
+ });
+ // ROUTES V0-1 END
 
  module.exports = router
