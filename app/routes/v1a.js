1a12f2a087f53b64e90b76e2f3cf52948ae961bb
Richard Smith <richard.smith1@engineering.digital.dwp.gov.uk>
2021-03-11 16:36:51 +0000

    Adding not eligible routes and to screen and tidying up not used files

--- app/routes/v1a.js
+++ app/routes/v1a.js
@@ -8,29 +8,38 @@ module.exports = function (router) {
         }
     });
 
-    router.post('/v1a/special-rules-ds1500', (req, res, next) => {
-        const specialRules = req.session.data['terminally-ill'];
-        if (specialRules === 'Yes') {
-            res.redirect('/v1a/special-rules-eligible');
-        } else {
-            res.redirect('/v1a/special-rules-eligible');
-        }
-    });
+//    router.post('/v1a/special-rules-ds1500', (req, res, next) => {
+//         const specialRules = req.session.data['terminally-ill'];
+//         if (specialRules === 'Yes') {
+//             res.redirect('/v1a/special-rules-eligible');
+//         } else {
+//             res.redirect('/v1a/special-rules-eligible');
+//         }
+//     });
 
 
     router.post('/v1a/health-condition', (req, res, next) => {
         const healthCondition = req.session.data['condition'];
-        if (healthCondition === 'No') {
+        if (healthCondition === 'Yes') {
+            res.redirect('/v1a/over-3-months');
+        } else {
+            res.redirect('/v1a/not-eligible-health-condition');
+        }
+    });
+
+    router.post('/v1a/over-9-months', (req, res, next) => {
+        const overMonths = req.session.data['over-9-months'];
+        if (overMonths === 'No') {
             res.redirect('/v1a/over-16');
         } else {
-            res.redirect('/v1a/over-3-months');
+            res.redirect('/v1a/not-eligible-health-condition-9-months');
         }
     });
 
     router.post('/v1a/over-16', (req, res, next) => {
         const over16 = req.session.data['over-16'];
         if (over16 === 'Answer 1') {
-            res.redirect('/v1a/not-eligible');
+            res.redirect('/v1a/not-eligible-under-16');
         } else if (over16 === 'Answer 2'){
             res.redirect('/v1a/where-you-live');
         } else {
@@ -43,7 +52,7 @@ module.exports = function (router) {
         if (overSpa === 'Yes') {
             res.redirect('/v1a/over-spa-payment-question');
         } else {
-            res.redirect('/v1a/where-you-live');
+            res.redirect('/v1a/not-eligible-over-spa');
         }
     });
 
@@ -56,31 +65,31 @@ module.exports = function (router) {
         }
     });
 
+    router.post('/v1a/living-in-gb-further-question', (req, res, next) => {
+        const immigrationControl = req.session.data['immigration-control'];
+        if (immigrationControl === 'No') {
+            res.redirect('/v1a/living-in-gb');
+        } else {
+            res.redirect('/v1a/not-eligible-immigration');
+        }
+    });
+
     router.post('/v1a/living-in-gb', (req, res, next) => {
         const livingGb = req.session.data['gb'];
         if (livingGb === 'Yes') {
-            res.redirect('/v1a/eligible');
+            res.redirect('/v1a/next-page');
         } else {
-            res.redirect('/v1a/living-in-gb-further-question');
+            res.redirect('/v1a/living-in-gb-further-question-2');
         }
     });
-//     router.post('/v1/not-eligible', (req, res, next) => {
-//         const destination = req.session.data['destination'];
-//         if (destination === 'health-condition') {
-//             delete req.session.data.destination
-//             res.redirect('/v1/health-condition');
-//         } else if (destination === 'over-3-months') {
-//             delete req.session.data.destination
-//             res.redirect('/v1/over-3-months');
-//         } else if (destination === 'over-9-months') {
-//             delete req.session.data.destination
-//             res.redirect('/v1/over-9-months');
-//         } else if (destination === 'where-you-live') {
-//             delete req.session.data.destination
-//             res.redirect('/v1/where-you-live');
-//         } else {
-//             delete req.session.data.destination
-//             res.redirect('/v1/check-answers-1');
-//         }
-//     });
+
+    router.post('/v1a/living-in-gb-further-question-2', (req, res, next) => {
+        const immigrationControl2 = req.session.data['immigration-control-2'];
+        if (immigrationControl2 === 'No') {
+            res.redirect('/v1a/next-page');
+        } else {
+            res.redirect('/v1a/not-eligible-immigration');
+        }
+    });
+
  };
