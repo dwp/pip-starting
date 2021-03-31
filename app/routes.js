0be3bbecd760b7cae0faf4fb78332117acf843bb
Richard Smith <richard.smith1@engineering.digital.dwp.gov.uk>
2021-03-31 09:11:57 +0100

    Setting up v2

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,7 @@ const v1 = require('./routes/v1')
 const v1a = require('./routes/v1a')
 const v1b = require('./routes/v1b')
 const v1c = require('./routes/v1c')
+const v2 = require('./routes/v2')
 
 // ROUTES REDIRECT END
 
@@ -20,6 +21,7 @@ v1(router);
 v1a(router);
 v1b(router);
 v1c(router);
+v2(router);
 // ROUTER LIST END
 
 module.exports = router
