66c37e17fb70ba1fbaca3bfa6379bd103d510e63
James Ashcroft <james.ashcroft@engineering.digital.dwp.gov.uk>
2022-06-08 19:16:15 +0100

    Changes to alternative tasklist journey
    Edited the journey by adding more pages and editing content
    Added routes to make radio buttons functional

--- app/routes/alternative-tasklist.js
+++ app/routes/alternative-tasklist.js
@@ -1,25 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
+module.exports = function (router) {
+
+router.post('/experimental/alternative_tasklist_idea/another', function(req, res) {
+    if (req.session.data['condition2'] == "yes") {
+               res.redirect('/experimental/alternative_tasklist_idea/condition')
+             } else {
+               res.redirect('/experimental/alternative_tasklist_idea/check')
+             }
+})
+
+
 // route for checking if citizen needs to claim for an activity
 router.post('/experimental/alternative_tasklist_idea/activity_hints/preparing-food-hint', (req, res, next) => {
           // Checks radio options, if "Add a new caseload" is selected, route to "create-new-case-ready-review" page.
-         if (req.session.data['prep-food-help'] == "yes") {
+         if (req.session.data['preparing-food-help'] == "yes") {
            // redirect to "create-new-case-ready-review" page.
            res.redirect('/experimental/alternative_tasklist_idea/preparing_food/preparing-food-detail')
          } else {
-           // redirect to "ready-for-review-success" page.
-           res.redirect('/version-one/minimum-viable-product/case-selector/ready-for-review-success')
+           //redirect to "ready-for-review-success" page.
+           res.redirect('/experimental/alternative_tasklist_idea/eating_and_drinking/eating-and-drinking-detail')
          }
 })
-
-router.post('/experimental/alternative_tasklist_idea/another', function(req, res) {
-
-  let condition2 = req.session.data['condition2']
-
-  if (condition2 === 'Yes') {
-      res.redirect('condition-2')
-    } else {
-      res.redirect('check')
-    }
-})
+}
