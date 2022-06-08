93122e78fa677eee8bdbaeada5afd41f4bbddb67
James Ashcroft <james.ashcroft@engineering.digital.dwp.gov.uk>
2022-06-08 19:26:48 +0100

    Updated routes on alternative-tasklist.js

--- app/routes/alternative-tasklist.js
+++ app/routes/alternative-tasklist.js
@@ -4,6 +4,14 @@ const router = express.Router()
 
 module.exports = function (router) {
 
+  router.post('/experimental/alternative_tasklist_idea/another', function(req, res) {
+      if (req.session.data['condition2'] == "yes") {
+                 res.redirect('/experimental/alternative_tasklist_idea/condition')
+               } else {
+                 res.redirect('/experimental/alternative_tasklist_idea/check')
+               }
+  })
+
 // route for checking if citizen needs to claim for an activity
 router.post('/experimental/alternative_tasklist_idea/activity_hints/preparing-food-hint', (req, res, next) => {
           // Checks radio options, if "Add a new caseload" is selected, route to "create-new-case-ready-review" page.
